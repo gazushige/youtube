45aeb21b4cfb5669d23f7dd6722c00482d05bbc6
gazushige <gazushige@gmail.com>
2021-11-14 01:57:27 +0900

    キャンセル時の処理、initialfile追記

--- youtube.py
+++ youtube.py
@@ -1,6 +1,8 @@
 from pytube import YouTube
-import tkinter 
+import tkinter
 import tkinter.filedialog as PATH
+
+
 def movieCode():
     # Tkクラス生成
     root = tkinter.Tk()
@@ -16,28 +18,41 @@ def movieCode():
     # テキストボックス
     txt = tkinter.Entry(width=50)
     txt.place(x=80, y=70)
-    
+
     # 説明
-    description=tkinter.Label(text='YouTubeの動画URLをコピペして\nダウンロードボタンを押して下さい')
-    description.place(x=120,y=100)
+    description = tkinter.Label(text='YouTubeの動画URLをコピペして\nダウンロードボタンを押して下さい')
+    description.place(x=120, y=100)
     # 決定ボタン
-    btn = tkinter.Button(root, text='ダウンロード', command=lambda:btnClick(txt.get()))
+    btn = tkinter.Button(root, text='ダウンロード',
+                         command=lambda: btnClick(txt.get()))
     btn.place(x=160, y=170)
-    
-    # 表示  
+
+    # 表示
     root.mainloop()
-    return 
+    return
+
 
 def btnClick(url):
-    # ダウンロードしたい動画のURLをいれて取得
-    # txt = "URLをコピペしてください"
-
-    savePath=PATH.asksaveasfilename()
     yt = YouTube(url)
+
     # 最高画質のmp4形式でダウンロード
     stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+
+    # デフォルトのファイル名を取得
+    fn = stream.default_filename
+
+    # 名前をつけて保存のダイアログボックスを表示
+    savePath = PATH.asksaveasfilename(initialfile=fn, initialdir='./')
+
+    # キャンセル時の処理
+    if not savePath:
+        print('キャンセルされたので処理を中断します')
+        return
+
+    # ダウンロード実施
     stream.download(savePath)
     return
+
 
 if __name__ == '__main__':
     movieCode()
